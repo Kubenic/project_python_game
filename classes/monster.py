f1aadeabea913e74d2ca4af8c67e14e3c0ed0865
apayen <adrien.payen1@gmail.com>
2017-07-27 13:22:33 +0200

    add monster and attack

--- classes/monster.py
+++ classes/monster.py
@@ -1,11 +1,9 @@
 import pygame
 
 
-class Player(pygame.sprite.Sprite):
+class Monster(pygame.sprite.Sprite):
     def __init__(self, position):
-        self.position = position
-
-        self.sheet = pygame.image.load('DawnLike/Commissions/Warrior.png');
+        self.sheet = pygame.image.load('DawnLike/Characters/Undead0.png');
         self.sheet.set_clip(0, 0, 16, 16)
         self.image = self.sheet.subsurface(self.sheet.get_clip())
         self.rect = self.image.get_rect()
@@ -15,15 +13,6 @@ class Player(pygame.sprite.Sprite):
         self.right_states = {0: (0, 32, 16, 16), 1: (16, 32, 16, 16), 2: (32, 32, 16, 16), 3: (48, 32, 16, 16)}
         self.up_states = {0: (0, 48, 16, 16), 1: (16, 48, 16, 16), 2: (32, 48, 16, 16), 3: (48, 48, 16, 16)}
         self.down_states = {0: (0, 0, 16, 16), 1: (16, 0, 16, 16), 2: (32, 0, 16, 16), 3: (48, 0, 16, 16)}
-
-        self.status = "standing"
-        self.arm = pygame.image.load('DawnLike/Items/MedWep.png')
-        self.arm.set_clip(0, 0, 16, 16)
-        self.arm_img = self.arm.subsurface(self.arm.get_clip())
-        self.arm_rect = self.image.get_rect()
-        self.arm_rect.topleft = tuple(x-y for x, y in zip(position, (20, 0)))
-
-        self.orientation = "left"
 
     def get_frame(self, frame_set):
         self.frame += 1
@@ -42,25 +31,15 @@ class Player(pygame.sprite.Sprite):
         if direction == 'left':
             self.clip(self.left_states)
             self.rect.x -= 5
-            self.arm_rect.x -= 5
-            self.arm_rect.topleft = tuple(x - y for x, y in zip(self.rect.topleft, (16, 0)))
-            self.arm_img = self.arm.subsurface(self.arm.get_clip())
-
         if direction == 'right':
             self.clip(self.right_states)
             self.rect.x += 5
-            self.arm_rect.x += 5
-            self.arm_rect.topleft = tuple(x - y for x, y in zip(self.rect.topleft, (-16, 0)))
-            self.arm_img = pygame.transform.flip(self.arm.subsurface(self.arm.get_clip()), 1, 0)
-
         if direction == 'up':
             self.clip(self.up_states)
             self.rect.y -= 5
-            self.arm_rect.y -= 5
         if direction == 'down':
             self.clip(self.down_states)
             self.rect.y += 5
-            self.arm_rect.y += 5
 
         if direction == 'stand_left':
             self.clip(self.left_states[0])
@@ -72,14 +51,6 @@ class Player(pygame.sprite.Sprite):
             self.clip(self.down_states[0])
 
         self.image = self.sheet.subsurface(self.sheet.get_clip())
-
-    def attack(self):
-        self.status = "attacking"
-
-        if self.orientation == "left":
-            self.arm_img = pygame.transform.rotate(self.arm.subsurface(self.arm.get_clip()), 40)
-        if self.orientation == "right":
-            self.arm_img = pygame.transform.rotate(self.arm.subsurface(self.arm.get_clip()), -120)
 
     def handle_event(self, event):
         if event.type == pygame.QUIT:
@@ -99,10 +70,6 @@ class Player(pygame.sprite.Sprite):
             if event.key == pygame.K_DOWN:
                 self.update('down')
 
-            # Attack
-            if event.key == pygame.K_SPACE:
-                self.attack()
-
         if event.type == pygame.KEYUP:
 
             # Deplacement
@@ -115,10 +82,21 @@ class Player(pygame.sprite.Sprite):
             if event.key == pygame.K_DOWN:
                 self.update('stand_down')
 
-            # Attack
-            if event.key == pygame.K_SPACE:
-                self.status = "standing"
-                if self.orientation == "left":
-                    self.arm_img = pygame.transform.rotate(self.arm.subsurface(self.arm.get_clip()), 0)
-                if self.orientation == "right":
-                    self.arm_img = pygame.transform.rotate(self.arm.subsurface(self.arm.get_clip()), -90)
+    def moove(self):
+        # Before main loop
+        human_files = ["human1.png", "human2.png"]
+        human_sprites = [pygame.image.load(filename).convert_alpha() for filename in human_files]
+        human1_index = 0
+
+        ...
+
+        # During main loop
+        if (human1_position.left <= 555):
+            human1_position = human1_position.move(2, 0)  # move first human
+            human1_index = (human_index + 1) % len(human_sprites)  # change sprite
+        else:
+            move = STOP
+            human1_index = 0
+        human1 = human_sprites[human1_index]
+        screen.blit(human1, human1_position)
+        pygame.display.update()
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pygame, sys, json
 from pprint import pprint
 from pygame import *
 from classes.player import Player
+from classes.monster import Monster
 
-with open('map.json') as data_file:    
-	data = json.load(data_file)
+with open('map.json') as data_file:
+    data = json.load(data_file)
 
 print(len(data["map"][0]))
 
-joueur = Player((300,200))
+joueur = Player((300, 200))
+monster = Monster((400, 300))
 pygame.init()
 
 tileswidthNumber = 50
@@ -16,31 +18,38 @@ tilesHeightNumber = 50
 tilesHeightSize = 16
 tilesWidthSize = 16
 
-screenWidth = tilesWidthSize*tileswidthNumber
-screenHeight = tilesHeightSize*tilesHeightNumber
+screenWidth = tilesWidthSize * tileswidthNumber
+screenHeight = tilesHeightSize * tilesHeightNumber
 
 fenetre = pygame.display.set_mode((screenWidth, screenHeight))
-#Icone
+
+# Icone
 image_icone = "DawnLike/Commissions/Icons.png"
 icone = pygame.image.load(image_icone)
 pygame.display.set_icon(icone)
-#Titre
+
+# Titre
 titre_fenetre = "Super jeux !"
 pygame.display.set_caption(titre_fenetre)
 
-
 clock = pygame.time.Clock()
-#BOUCLE PRINCIPALE
+
+# BOUCLE PRINCIPALE
 continuer = 1
 while continuer:
-	fenetre.fill((0,0,0))
-	clock.tick(60)
+    fenetre.fill((0, 0, 0))
+    clock.tick(60)
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-	joueur.handle_event(event)
-	fenetre.blit(joueur.image, joueur.rect)
-	pygame.display.update();
-	continue
+    fenetre.blit(pygame.transform.scale(monster.image, (30, 30)), monster.rect)
+
+    joueur.handle_event(event)
+    fenetre.blit(pygame.transform.scale(joueur.image, (30, 30)), joueur.rect)
+    fenetre.blit(pygame.transform.scale(joueur.arm_img, (30, 30)), joueur.arm_rect)
+
+
+    pygame.display.update();
+    continue
